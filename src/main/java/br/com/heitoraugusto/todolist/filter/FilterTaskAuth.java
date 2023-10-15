68ddef074cba1db384b083fa627127f9f61a7cf0
Heitor Augusto <heitor.augusto@fabricadenegocio.com.br>
2023-10-15 09:12:16 -0300

    Init commit

---
+++ src/main/java/br/com/heitoraugusto/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,65 @@
+package br.com.heitoraugusto.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.heitoraugusto.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if (!servletPath.startsWith("/tasks/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        var authorization = request.getHeader("Authorization");
+        if (authorization == null) {
+            response.sendError(401);
+            return;
+        }
+
+        var authorizationCode = authorization.substring("Basic".length()).trim();
+        byte[] authDecoded = Base64.getDecoder().decode(authorizationCode);
+        var authString = new String(authDecoded);
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(401);
+            return;
+        }
+
+        var isPasswordValid = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (!isPasswordValid.verified) {
+            response.sendError(401);
+            return;
+        }
+
+        request.setAttribute("idUser", user.getId());
+
+        filterChain.doFilter(request, response);
+        return;
+    }
+
+}
---
+++ src/main/java/br/com/heitoraugusto/todolist/task/ITaskRepository.java
@@ -0,0 +1,14 @@
+package br.com.heitoraugusto.todolist.task;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+
+    Optional<TaskModel> findOneByIdAndIdUser(UUID id, UUID idUser);
+
+}
